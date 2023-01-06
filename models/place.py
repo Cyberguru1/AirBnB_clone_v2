91c5247faa53c7c89a9238c3a6defefa4b4140fb
Kenza1525 <kerianenzabampema@gmail.com>
2023-01-07 01:13:51 +0200

    Modified place.py added Base as a parameter

--- models/place.py
+++ models/place.py
@@ -5,7 +5,7 @@ from models.base_model import BaseModel, Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Float
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
 
     __tablename__ = "places"
@@ -19,4 +19,3 @@ class Place(BaseModel):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-    amenity_ids = []
