7821453f633240c5ac3c321b534e6769e9b51b5b
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-10 07:17:36 -0600

    update

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,71 +14,71 @@ from models.amenity import Amenity
 
 
 class DBStorage:
-    """ create tables in the current enviroment"""
-    __engine = None
-    __session = None
+	""" create tables in the current enviroment"""
+	__engine = None
+	__session = None
 
-    def __init__(self):
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
-        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                     format(user,
-                                     passwd, host, db), pool_pre_ping=True)
+	def __init__(self):
+		user = getenv("HBNB_MYSQL_USER")
+		passwd = getenv("HBNB_MYSQL_PWD")
+		db = getenv("HBNB_MYSQL_DB")
+		host = getenv("HBNB_MYSQL_HOST")
+		env = getenv("HBNB_ENV")
+		self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+			.format(user, passwd, 
+			host, db), pool_pre_ping=True)
 
-        if env == "test":
-            Base.metadata.drop_all(self.__engine)
+		if env == "test":
+			Base.metadata.drop_all(self.__engine)
 
-    def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of __object
-        """
-        dic = {}
-        if cls:
-            if type(cls) is str:
-                cls = eval(cls)
-            query = self.__session.query(cls)
-            for elem in query:
-                key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
-        else:
-            lists = [State, City, User, Place, Review, Amenity]
-            for attr in lists:
-                query = self.__session.query(attr)
-                for elem in query:
-                    key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-        return (dic)
+	def all(self, cls=None):
+		"""returns a dictionary
+		Return:
+			returns a dictionary of __object
+		"""
+		dic = {}
+		if cls:
+			if type(cls) is str:
+				cls = eval(cls)
+			query = self.__session.query(cls)
+			for elem in query:
+				key = "{}.{}".format(type(elem).__name__, elem.id)
+				dic[key] = elem
+		else:
+			lists = [State, City, User, Place, Review, Amenity]
+			for attr in lists:
+				query = self.__session.query(attr)
+				for elem in query:
+					key = "{}.{}".format(type(elem).__name__, elem.id)
+					dic[key] = elem
+		return (dic)
 
-    def new(self, obj):
-        """add a new element in the table
-        """
-        self.__session.add(obj)
+	def new(self, obj):
+		"""add a new element in the table
+		"""
+		self.__session.add(obj)
 
-    def save(self):
-        """save modifications
-        """
-        self.__session.commit()
+	def save(self):
+		"""save modifications
+		"""
+		self.__session.commit()
 
-    def delete(self, obj=None):
-        """delete an element in the table
-        """
-        if obj:
-            self.session(obj)
+	def delete(self, obj=None):
+		"""delete an element in the table
+		"""
+		if obj:
+			self.session(obj)
 
-    def reload(self):
-        """ reload configs
-        """
-        Base.metadata.create_all(self.__engine)
-        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(sec)
-        self.__session = Session()
+	def reload(self):
+		""" reload configs
+		"""
+		Base.metadata.create_all(self.__engine)
+		sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+		Session = scoped_session(sec)
+		self.__session = Session()
 
-    def close(self):
-        """ closes the session
-        """
-        self.reload()
-        self.__session.close()
+	def close(self):
+		""" closes the session
+		"""
+		self.reload()
+		self.__session.close()
