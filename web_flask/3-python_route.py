2ac8cd6c6a7caf58b1a25c3cc878439ad166500f
Cyberguru1 <hamzacypher34@gmail.com>
2023-03-10 06:33:15 -0600

    update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -4,8 +4,8 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /: Displays 'Hello HBNB!'.
     /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>,
-    /python/(<text>): Displays 'Python' followed by <text>.
+    /c/<text>: Displays 'C' followed by the value of <text>.
+    /python/(<text>): Displays 'Python' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -26,16 +26,22 @@ def hbnb():
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>."""
+    """Displays 'C' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+
 @app.route("/python", strict_slashes=False)
-@app.route("/python/(<text>)", strict_slashes=False)
-def c(text="is_cool"):
-    """Displays 'Python' followed by the value of <text>."""
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    """Displays 'Python' followed by the value of <text>.
+    Replaces any underscores in <text> with slashes.
+    """
     text = text.replace("_", " ")
     return "Python {}".format(text)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
